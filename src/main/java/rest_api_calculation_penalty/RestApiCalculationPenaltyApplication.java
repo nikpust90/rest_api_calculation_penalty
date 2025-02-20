b1e8eb02f97c2923d2265858631331d72920034c
nikpust90 <nikolai.first21@yandex.ru>
2025-02-20 10:16:42 +0300

    master

---
+++ src/main/java/rest_api_calculation_penalty/RestApiCalculationPenaltyApplication.java
@@ -0,0 +1,13 @@
+package rest_api_calculation_penalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCalculationPenaltyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCalculationPenaltyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rest_api_calculation_penalty/RestApiCalculationPenaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package rest_api_calculation_penalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCalculationPenaltyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
